5e6960cb608d477d8bb7628e346324fd9ed0b3cc
Sebastian Alexandersson <sebbe@sebbe.dev>
2019-11-23 23:52:46 +0100

    asdf

--- backend/api/db/db.js
+++ backend/api/db/db.js
@@ -2,7 +2,7 @@ const mariadb = require('mariadb');
 const db = () => {
 	return mariadb.createConnection({
 		host: 'db', 
-		user:'studentprojekt', 
+		user:'sebbe', 
 		password: 'asdf123!##',
 		database: 'studentprojekt',
 	});
